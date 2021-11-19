c8d0f80ece43157f82229ecfe1861a5e654cb3c1
Yussel Paredes <yussel.paredes@ultra.io>
2021-11-19 15:45:38 -0600

    Add store & wallet sample tests

--- tests/environment/environment.example.ts
+++ tests/environment/environment.example.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // App urls
-  launchScreenUrl: 'https://auth.dev.ultra.io/en/',
+  launchScreenUrl: 'https://launch-screen.dev.app.ultra.io/en/',
   universesNavUrl: 'https://universes-nav.dev.app.ultra.io/en/',
   walletUrl: 'https://wallet.dev.app.ultra.io/en/',
   storeUrl: 'https://dev.app.ultra.io/en/',
